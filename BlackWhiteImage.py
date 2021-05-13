af234aba1cf3cc527cd406b71304a580933cde32
Kayleen Liu <kayleenliu@Kayleens-MacBook-Pro.local>
2021-05-12 23:30:15 -0600

    version2

--- BlackWhiteImage.py
+++ BlackWhiteImage.py
@@ -1,5 +1,8 @@
 from PIL import Image
+import os 
 
-img = Image.open("img.jpeg")   # read image file
-img = img.convert("L")   # convert image to black and white
-img.save("newImg.jpeg")   # save as a new image file
+for file in os.listdir('original')
+    if file.endsWith('.jpeg')
+        img = Image.open(os.path.join('original',file))   # read image file
+        img = img.convert('L')   # convert image to black and white
+        img.save(os.path.join('new',file[:-4] + '_grey.jpeg'))   # save as a new image file
